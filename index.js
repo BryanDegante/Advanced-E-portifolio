8969c30e8cfd29b5af989659036b302813e905e1
Bryan <bdegante0906@gmail.com>
2024-07-28 18:16:54 -0500

    finished portfolio template

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-//template_lkqpnxm
-//service_ii2olnh
-//JEF6oOdWYGdzqxUFN
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape")
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY *scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i){
+        const isOdd = i % 2 !== 0; 
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform =     `translate( ${x*boolInt}px , ${y*boolInt}px) rotate(${x *boolInt *10}deg)`
+        
+    }
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+        console.log(" it worked")
+    }
+    else {
+        document.body.classList.remove("dark-theme"); 
+    }
+}
 
 function contact(event) {
     event.preventDefault();
@@ -24,7 +48,6 @@ function contact(event) {
         })
 }
 
-let isModalOpen = false;
 function toggleModal() {
     if (isModalOpen) {
         isModalOpen = false;
